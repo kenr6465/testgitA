695e7451ecf8b41ce37c9adff8db5216938a187b
kenr6465 <kenr6465@gmail.com>
2016-12-24 22:39:31 -0500

    Add files via upload

---
+++ w_api_to_file.php
@@ -0,0 +1,14 @@
+﻿    <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // collect value of input field
+            $my_api_fp = $_POST['api_fp']; 
+            $my_local_fp = $_POST['local_fp'];
+            if (!empty($my_local_fp) && !empty($my_api_fp)) {
+                $myData = file_get_contents($my_api_fp);
+                file_put_contents($my_local_fp, $myData);
+                echo "success";
+            } else {
+                echo "Something is wrong. No file saved. \n";
+            }
+        }
+    ?>
